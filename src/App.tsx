a269a14e8ae42e0d3a17bf27f608ffae76a32222
wondonghwi <ehdehd1289@naver.com>
2023-06-04 09:48:18 +0900

    eslint test를 위한 임시 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [state, setState] = useState(null);
-
-  const temp = useCallback(() => {
-    if (state === null) {
-      return;
-    }
-  }, []);
-
   return (
     <div className='App'>
       <header className='App-header'>
